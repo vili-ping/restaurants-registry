57d4291d2aed51526922a1674b3c046e8fa6fe42
vili-ping <drdlemmer@gmail.com>
2025-01-22 00:54:33 +0300

    Add tabs for restaurants

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import { Restaurant } from './Restaurant';
 
 export const Restaurants = ({ restaurants }) => {
+  const [currentRestaurant, setCurrentRestaurant] = useState(restaurants[0]);
+
+  const changeRestaurant = (id) => {
+    setCurrentRestaurant(
+      restaurants.find((restaurant) => restaurant.id === id)
+    );
+  };
+
   return (
     <>
-      {restaurants.map(({ id, name, menu, reviews }) => (
-        <Restaurant
-          key={id}
-          name={name}
-          menu={menu}
-          reviews={reviews}
-        />
+      {restaurants.map((restaurant) => (
+        <button
+          key={restaurant.id}
+          onClick={() => changeRestaurant(restaurant.id)}
+        >
+          {restaurant.name}
+        </button>
       ))}
+
+      <Restaurant
+        key={currentRestaurant.id}
+        name={currentRestaurant.name}
+        menu={currentRestaurant.menu}
+        reviews={currentRestaurant.reviews}
+      />
     </>
   );
 };
