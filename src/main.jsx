91a51362cbf9968d3db562216ecd921fc3e10e2d
vili-ping <drdlemmer@gmail.com>
2025-01-28 00:23:28 +0300

    Turn on StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+import { StrictMode } from 'react';
 
 const rootElement = document.getElementById('root');
 
 const reactRootElement = createRoot(rootElement);
 
-reactRootElement.render(<App />);
+reactRootElement.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
